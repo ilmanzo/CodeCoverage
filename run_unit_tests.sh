3a0c9ccebf35226381595c21a61d02c8bd615c3b
Andrea Manzini <ilmanzo@gmail.com>
2025-07-02 08:58:06 +0200

    aggregate Python tools in a Go program

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Running Python unit tests..."
+echo "Running Go unit tests..."
+pushd cmd
+go test -v ./...
+popd
 export PIN_ROOT="${PIN_ROOT:-/var/coverage/pin}"
 pushd tests || exit 1
-python3 test_coverage_analyzer.py
-sudo -E python3 test_wrap.py
 echo "Building and running C++ unit tests..."
 CXXFLAGS=$(pkg-config --cflags catch2 2>/dev/null || echo "")
 LDFLAGS=$(pkg-config --libs catch2 2>/dev/null || echo "")
